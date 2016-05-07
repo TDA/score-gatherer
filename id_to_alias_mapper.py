7f23cd464ef12b23de0294e416633e2687f24b58
Sai Chandramouli <schand31@asu.edu>
2016-05-07 00:19:45 -0600

    stupid timezone problem

--- id_to_alias_mapper.py
+++ id_to_alias_mapper.py
@@ -14,14 +14,13 @@ def id_alias_mapper(filename):
 
 if __name__ == '__main__':
     id_alias_map = id_alias_mapper("id_alias_score")
-    # print(id_alias_map)
     id_list_sorted = sorted(id_alias_map.keys())
-    # print(id_list_sorted)
     ordered_score = OrderedDict()
     for id in id_list_sorted:
         alias = id_alias_map.get(id)
         score = d.get(alias)
         print(score)
+        # this now has everything required
         ordered_score[id] = score
 
 
